259fe80aaacbc974b6c9253b66591275c7d8c343
sarthakjana09 <sarthakjana09@gmail.com>
2025-05-29 12:58:25 +0530

    Implemneted event publishing

--- order-service/order-domain/order-application-service/src/main/java/com/food/order/service/domain/OrderTrackCommandHandler.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/order/service/domain/OrderTrackCommandHandler.java
@@ -10,6 +10,6 @@ import org.springframework.stereotype.Component;
 public class OrderTrackCommandHandler {
 
     TrackOrderResponse trackOrderOrder(TrackOrderQuery trackOrderQuery) {
-
+        return null;
     }
 }
